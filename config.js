3ac45594bf77fedf3905e7d8cdacb98352c19050
Daniel Parucci Somerlate <89330408+Paruccikk@users.noreply.github.com>
2025-02-01 15:42:18 -0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    BOT_NOME: "Daniela",
-    MENSAGEM_BOAS_VINDAS: "Oie! Eu sou a Daniela. Para começarmos, como posso te chamar?",
-    MENSAGEM_DESCONHECIDA: "Desculpe, não entendi sua pergunta. Poderia reformular?",
-    MENSAGEM_ERRO: "Ocorreu um erro ao processar sua mensagem. Tente novamente mais tarde."
-};
