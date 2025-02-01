6ece2ed271c704f82aa11bf47bd910d63ee44da3
Daniel Parucci Somerlate <89330408+Paruccikk@users.noreply.github.com>
2025-02-01 15:43:42 -0300

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Instala as dependências necessárias para o Puppeteer no Render
-apt-get update
-apt-get install -y wget ca-certificates fonts-liberation libappindicator3-1 libnss3 lsb-release xdg-utils
-
-# Instalar o Chromium (caso não esteja disponível)
-apt-get install -y chromium
