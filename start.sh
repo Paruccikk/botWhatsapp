7fc3665f359e9ba2b37f78db47bb984c54649e54
Daniel Parucci Somerlate <89330408+Paruccikk@users.noreply.github.com>
2025-02-01 22:13:40 -0300

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Instalar as dependências, caso não estejam instaladas
-echo "Instalando dependências..."
-npm install
-
-# Iniciar o servidor
-echo "Iniciando o servidor..."
-node ./src/js/server.js
